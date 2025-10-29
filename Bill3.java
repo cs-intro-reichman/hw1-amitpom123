0392d992af1e1e236e44dd3ca7b668e279b20d66
amitpom123 <amitpom123@gmail.com>
2025-10-29 19:46:50 +0200

    completed

--- Bill3.java
+++ Bill3.java
@@ -2,7 +2,12 @@
 public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
-	    String name1 = args[0];
-	    // Replace this comment with the rest of your code   
+	    String name1 = args[0];      //last name to print
+	    String name2 = args[1];      //second name to print
+		String name3 = args[2];      //first name to print
+		int sum = Integer.parseInt(args[3]);     //the bill
+		double x = (double)sum / 3;                      //the bill is now div. by 3
+		x = Math.ceil(x);                        //rounding up as asked
+		System.out.println("Dear" + " " + name3 + "," + " " + name2 + "," + " " + "and" + " " + name1 + ":" + " pay" + " " + x + " Shekels each.");
 	}
 }
