8308d0be9ff1b78f651c9cc4c9b226db757c2b48
amitpom123 <amitpom123@gmail.com>
2025-10-30 14:24:54 +0200

    NumWords finished

--- NumWords.java
+++ NumWords.java
@@ -1,10 +1,10 @@
 // Prints a given number using a hundreds, tens, and units notation.
 public class NumWords {
 	public static void main(String args[]) {
-	    int x = 517;
+	    int x = Integer.parseInt(args[0]);
 		int Hundreds = x / 100;
 		int Tens = (x % 100) / 10;
 		int Ones = x % 10;
-		System.out.println(Hundreds + " " + "hundreds" + "," + Tens + " " + "tens" + "," + Ones + " " + "ones");
+		System.out.println(Hundreds + " " + "hundreds" + "," + Tens + " " + "tens" + "," + " and " + Ones + " " + "ones.");
 	}
 }
