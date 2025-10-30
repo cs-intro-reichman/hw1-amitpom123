09c1364a240df058ea6a64b238e3c4be5daad558
amitpom123 <amitpom123@gmail.com>
2025-10-30 18:09:45 +0200

    added space

--- NumWords.java
+++ NumWords.java
@@ -5,6 +5,6 @@ public class NumWords {
 		int Hundreds = x / 100;
 		int Tens = (x % 100) / 10;
 		int Ones = x % 10;
-		System.out.println(Hundreds + " " + "hundreds" + "," + Tens + " " + "tens" + "," + " and " + Ones + " " + "ones.");
+		System.out.println(Hundreds + " " + "hundreds" + ", " + Tens + " " + "tens" + "," + " and " + Ones + " " + "ones.");
 	}
 }
